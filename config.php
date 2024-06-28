84feec8b5c15afe376269c0e16aab55333877175
coldwater-10 <91764021+coldwater-10@users.noreply.github.com>
2024-06-28 16:02:24 +0330

    Update config.php

--- config.php
+++ config.php
@@ -7,4 +7,9 @@ $subscription_urls = [
   "https://raw.githubusercontent.com/coldwater-10/V2rayCollectorVpnclashfa/main/vless_iran.txt",
   "https://raw.githubusercontent.com/Everyday-VPN/Everyday-VPN/main/subscription/main.txt",
   "https://raw.githubusercontent.com/Everyday-VPN/Everyday-VPN/main/subscription/test.txt",
+  "https://links.achaemenidempireofpersia.uk/cR3Q5X#IrancpinetSub1",
+  "https://sub1.achaemenidempireofpersia.uk/#IrancpinetSub2",
+  "https://raw.githubusercontent.com/jahangirGH/sublinks/main/Fragsubhiddify.txt#Irancpi_Hiddify_Fragment",
+  "https://raw.githubusercontent.com/Surfboardv2ray/Subs/main/Raw",
+  "https://raw.githubusercontent.com/Surfboardv2ray/Subs/main/Realm",
 ];
