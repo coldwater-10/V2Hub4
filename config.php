84df3d8122843043c88c47e7552aa866f900fbac
coldwater-10 <91764021+coldwater-10@users.noreply.github.com>
2024-06-28 15:43:35 +0330

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$subscription_urls = [
+  "https://raw.githubusercontent.com/coldwater-10/V2rayCollectorVpnclashfa/main/vmess_iran.txt",
+  "https://raw.githubusercontent.com/coldwater-10/V2rayCollectorVpnclashfa/main/ss_iran.txt",
+  "https://raw.githubusercontent.com/coldwater-10/V2rayCollectorVpnclashfa/main/trojan_iran.txt",
+  "https://raw.githubusercontent.com/coldwater-10/V2rayCollectorVpnclashfa/main/vless_iran.txt",
+  "https://raw.githubusercontent.com/Everyday-VPN/Everyday-VPN/main/subscription/main.txt",
+  "https://raw.githubusercontent.com/Everyday-VPN/Everyday-VPN/main/subscription/test.txt",
+];
